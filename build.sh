8bdc05c239a7587ab675341ba0970feab0abf91a
Sebastian Reimers <hallo@studio-link.de>
2018-01-19 17:53:10 +0100

    add travis and build

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+flac="1.3.2"
+
+mkdir -p mingw
+pushd mingw
+mingwurl="https://github.com/Studio-Link-v2/mingw/releases/download/v18.01.0"
+wget -N $mingwurl/cloog-0.18.5-1-x86_64.pkg.tar.xz
+wget -N $mingwurl/isl-0.18-3-x86_64.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-binutils-2.29-1-x86_64.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-configure-0.1-1-any.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-crt-5.0.3-1-any.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-gcc-7.2.1.20171224-1-x86_64.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-headers-5.0.3-1-any.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-pkg-config-2-3-any.pkg.tar.xz
+wget -N $mingwurl/mingw-w64-winpthreads-5.0.3-1-any.pkg.tar.xz
+wget -N $mingwurl/osl-0.9.1-1-x86_64.pkg.tar.xz
+yes | LANG=C sudo pacman -U *.pkg.tar.xz
+popd
+
+wget http://downloads.xiph.org/releases/flac/flac-${flac}.tar.xz
+tar -xf flac-${flac}.tar.xz
+ln -s flac-${flac} flac
+
+mkdir flac/build_win
+pushd flac/build_win
+${_arch}-configure --disable-ogg --enable-static --disable-cpplibs
+make
+popd
